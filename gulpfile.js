1b0526b39784f32c40876535746242c56750047f
jonocr <jono.calvo@gmail.com>
2020-04-06 19:32:32 -0700

    init project

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync');
+//const rename = require('gulp-rename');
+
+//compile scss into css
+function style() {
+    // 1. Where is my cscc file
+    return gulp.src('./src/scss/**/*.scss')
+        // 2. Pass that file into the css compiler
+        .pipe(sass({
+            //Minified css
+            outputStyle : 'compressed'
+        }).on('error', sass.logError))//Rename file to .min.css /.pipe(rename({ sufix: '.min'}))    
+        // 3. Where do I save the compile css
+        .pipe(gulp.dest('./src'))
+        // 4. Stream Changes to all browsers
+        .pipe(browserSync.stream());
+}
+
+function watch() {
+  
+    // browserSync.init({
+    //     server: {
+    //         baseDir: './'
+    //     }
+    // });
+   
+    // If a change happens on files then calls functions to refresh or reload
+    gulp.watch('./src/scss/**/*.scss', style);
+    //gulp.watch('./*.html').on('change', browserSync.reload);
+    //gulp.watch('./js/**/*.js').on('change', browserSync.reload);
+}
+
+exports.style = style;
+exports.watch = watch;
--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './main.css';
+// import logo from './logo.svg';
+// import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <header className="App-header">
+        Homepage
+      </header> */}
+      <section>
+        <nav className="side-menu">
+          <div className="container">
+            <ul>
+              <li>
+                <a href="#">Dashboard</a>
+              </li>
+              <li>
+                <a href="#">Menu Link 1</a>
+              </li>
+              <li>
+                <a href="#">Menu Link 1</a>
+              </li>
+              <li>
+                <a href="#">Menu Link 1</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </section>
     </div>
   );
 }
