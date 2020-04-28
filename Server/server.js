a963bf012fd0c07f0d0c1a51f0cfb9346af1a04a
jonocr <jono.calvo@gmail.com>
2020-04-27 23:23:22 -0700

    change db connector to mongoose

--- Server/server.js
+++ Server/server.js
@@ -1,13 +1,37 @@
-const express = require ('express');
-const MongoClient = require('mongodb').MongoClient;
-const bodyParser = require('body-parser');
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
 
 const app = express();
+const port = process.env.PORT || 8000;
 
-const port = 8000;
+app.use(cors());
+app.use(express.json());
+const uri = process.env.ATLAS_URI;
 
-require('./src/routes')(app, {});
+console.log("Env variable: " + uri);
+
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+})
 
 app.listen(port, () => {
-    console.log("We are running on port: " + port);
+    console.log(`Server is running on port: ${port}`);
 })
+
+// MongoClient.connect(db.url, {
+//     useUnifiedTopology: true,
+//     useNewUrlParser: true,
+//     }, (err,  database) => {
+//         // const db = client.db;
+//     if (err) return console.log(err);
+
+//     require('./src/routes')(app, database);
+//     app.listen(port, () => {
+//         console.log(`Server is running on port: ${port}`);
+//     })
+// });
--- Server/src/routes/ticket_routes.js
+++ Server/src/routes/ticket_routes.js
@@ -1,6 +1,28 @@
 module.exports = function(app, db) {
     app.post('/tickets', (req, res) => {
-        //Create the ticket
-        res.send('Hello from the server');
+        
+        var today = new Date();
+        var dueDate = new Date();
+        dueDate.setDate(today.getDate()+5);
+
+        const ticket = { 
+            title: req.body.title, 
+            creation: today,
+            status: 'open',
+            priority: 1,
+            description: req.body.description,
+            category: req.body.category,
+            due: dueDate.toString(),
+            creator: "jono.calvo1@gmail.com",
+            relatedIssues: [],
+            ModifiedBy: "",
+            agentAssigned: "",
+            lastModified: "" 
+        };
+        db.collection('tickets').insert(ticket, (err, result) => {
+            err ? res.send ({ error : "An error has ocurred" }) : res.send (result.ops[0])   
+        })
     })
 }
+
+
