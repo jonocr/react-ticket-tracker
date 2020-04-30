3cc1e62efdb90429be94f132bf28faea458b1921
jonocr <jono.calvo@gmail.com>
2020-04-29 23:38:53 -0700

    bcrypt library added

--- Server/src/routes/user_routes.js
+++ Server/src/routes/user_routes.js
@@ -1,14 +1,21 @@
 const router = require('express').Router();
 const User = require('../models/user.model');
+const bcrypt = require('bcrypt');
 
 router.route('/').get((req, res) => {
     User.find()
         .then(users => res.json(users))
-        .catch( err => res.status(400).json(`Error: ${err}`));
+        .catch(err => res.status(400).json(`Error: ${err}`));
 });
 
-router.route('/add').post((req, res) => {
-    console.log("roles ", req.body);
+router.route('/:userId').get((req, res) => {
+    const id = req.params.userId;
+    User.find()
+        .then(users => res.json(users))
+        .catch(err => res.status(400).json(`Error: ${err}`));
+});
+
+router.route('/signup').post((req, res, next) => {
     const data = req.body;
     const userName = data.username;
     const password = data.password;
@@ -17,13 +24,35 @@ router.route('/add').post((req, res) => {
     const manager = data.manager;
     const team = [];
 
-    const newUser = new User({
-        userName, password, email, roles, manager, team
+
+    bcrypt.hash(data.password, 10, (err, hash) => {
+        if (err) {
+            res.status(500).json({
+                error: err
+            })
+        } else {
+            console.log("hash passqord: ", hash);
+
+            bcrypt.compare(data.password, hash).then(function(result) {
+                // result == true
+                console.log("compare correct: ", result);
+            });
+            bcrypt.compare("someOtherPlaintextPassword", hash).then(function(result) {
+                console.log("compare : ", result);
+                // result == false
+            });
+            
+            const newUser = new User({
+                userName, password, hash, roles, manager, team
+            });
+
+        }
     });
 
-    newUser.save()
-        .then( () => res.json('User registered!'))
-        .catch( err => res.status(400).json(`Error: ${err}`));
+
+    // newUser.save()
+    //     .then( () => res.json('User registered!'))
+    //     .catch( err => res.status(400).json(`Error: ${err}`));
 
 
 });
