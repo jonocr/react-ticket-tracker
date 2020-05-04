afaee822c64423dd54ac1dc5ad6e778ca96ee9e8
jonocr <jono.calvo@gmail.com>
2020-05-04 12:58:30 -0700

    Dashboard Page and Userpage created

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,20 +1,25 @@
 import React, { useState } from 'react';
+import DashboardPage from './components/pages/DashboardPage';
 import './main.css';
-import TopBar from "./components/layout/TopBar";
-import SideMenu from "./components/layout/SideMenu";
-import NewTicket from "./components/others/NewTicket";
 
 function App() {
-  const [closeCss, setCloseCss] = useState("");
-
-  const clickToggle = (e) => {
-    closeCss === "" ? setCloseCss("close-menu") : setCloseCss("");
-    console.log("click!");
-  }
 
 
   return (
-    <div className={closeCss}>
+    <div>
+      <DashboardPage></DashboardPage>
+
+      {/* <div className="dashboard-main dashboard">
+        <UserList></UserList>
+      </div> */}
+
+    </div>
+  );
+}
+
+export default App;
+
+
       {/* <nav class="navbar navbar-expand-md navbar-light bg-light">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
@@ -26,69 +31,3 @@ function App() {
           </div>
         </div>
       </nav> */}
-      <SideMenu css={closeCss}></SideMenu>
-      <TopBar onClick={clickToggle} css={closeCss}></TopBar>
-
-      <div className="container">
-        <div className="dashboard-bar dashboard">
-          Dashboar Title
-        </div>
-        <div className="dashboard-main dashboard">
-          <div class="row">
-            <div class="col-12 col-md-6 col-lg-3">
-              <div className="box">
-                1 of 4
-              </div>
-            </div>
-            <div class="col-12 col-md-6 col-lg-3">
-              <div className="box">
-                2 of 4
-              </div>
-            </div>
-            <div class="col-12 col-md-6 col-lg-3">
-              <div className="box">
-                3 of 4
-              </div>
-            </div>
-            <div class="ccol-12 col-md-6 col-lg-3">
-              <div className="box">
-                4 of 4
-              </div>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-12 col-md-6 col-lg-3">
-              <div className="box">
-                1 of 4
-              </div>
-            </div>
-            <div class="col-12 col-md-6 col-lg-3">
-              <div className="box">
-                2 of 4
-              </div>
-            </div>
-            <div class="col-12 col-md-6 col-lg-3">
-              <div className="box">
-                3 of 4
-              </div>
-            </div>
-            <div class="ccol-12 col-md-6 col-lg-3">
-              <div className="box">
-                4 of 4
-              </div>
-            </div>
-          </div>
-
-        </div>
-
-        <div className="dashboard-main dashboard">
-          <NewTicket></NewTicket>
-        </div>
-      </div>
-
-
-    </div>
-  );
-}
-
-export default App;
---
+++ Client/src/components/pages/UserPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import UserList from '../users/UserList';
+
+const UserPage = (props) => {
+    return (
+        <div>
+
+            <UserList></UserList>
+        </div>
+    )
+}
+
+export default UserPage;
