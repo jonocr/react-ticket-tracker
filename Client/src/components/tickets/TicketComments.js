686d743ca9f4559561156b1e95390ea5a9e4e4da
jonocr <jono.calvo@gmail.com>
2020-07-10 15:33:29 -0700

    Comments Display changed

--- Client/src/components/tickets/TicketComments.js
+++ Client/src/components/tickets/TicketComments.js
@@ -6,8 +6,20 @@ const TicketComments = (props) => {
 			<div className="collapse" id="collapseExample">
 				{props.data.map((comment, index) => (
 					<div className="card card-body">
-						<p>{comment.email}</p>
-						<p>{comment.comment}</p>
+						{console.log("Comment: ", comment.lastModified.split("T")[1])}
+						<div className="row comment-head">
+							<div className="comment-author">
+								<strong>Author:</strong>
+								{comment.email}
+							</div>
+							<div className="comment-date">
+								<strong>Date:</strong>
+								{comment.lastModified.split("T")[0]}
+								&nbsp;
+								{comment.lastModified.split("T")[1].split(".")[0]}
+							</div>
+						</div>
+						<div className="comment-text">{comment.comment}</div>
 					</div>
 				))}
 			</div>
