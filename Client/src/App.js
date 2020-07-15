392187d98f30cb507712a2f29eaaba55bf35eb45
jonocr <jono.calvo@gmail.com>
2020-07-14 17:21:23 -0700

    Clean code delete unuse variables

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./main.css";
 
