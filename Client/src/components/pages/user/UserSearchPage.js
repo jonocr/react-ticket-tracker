85f7bda84e8fa6b6b6cbe96bef8016285b233e26
jonocr <jono.calvo@gmail.com>
2020-08-01 12:36:29 -0700

    Moved fetch to UserApi middelware

--- Client/src/components/pages/user/UserSearchPage.js
+++ Client/src/components/pages/user/UserSearchPage.js
@@ -4,8 +4,11 @@ import TopBar from "../../layout/TopBar";
 import SideMenu from "../../layout/SideMenu";
 import UserList from "../../users/UserList";
 import AuthContext from "../../utils/AuthContext";
+import { loadUsers } from "../../users/UserApi";
 
 const UserSearchPage = (props) => {
+	const abortController = new AbortController();
+	const signal = abortController.signal;
 	const { userData } = useContext(AuthContext);
 	const [closeCss, setCloseCss] = useState("");
 	const [users, setUsers] = useState([]);
@@ -19,27 +22,33 @@ const UserSearchPage = (props) => {
 		history.push(`/user/${user.email}`);
 	};
 
-	const loadUsers = () => {
-		fetch(`${process.env.REACT_APP_API_SERVER_URL}/users`, {
-			method: "GET",
-			headers: {
-				Authorization: `Bearer ${userData.token}`,
-				"Content-Type": "application/json",
-			},
-		})
-			.then((response) => {
-				return response.json();
-			})
-			.then((responseData) => {
-				setUsers(responseData);
-			})
-			.catch((err) => {
-				console.log("error at fetching: ", err);
-			});
-	};
+	// const loadUsers = () => {
+	// 	fetch(`${process.env.REACT_APP_API_SERVER_URL}/users`, {
+	// 		signal: signal,
+	// 		method: "GET",
+	// 		headers: {
+	// 			Authorization: `Bearer ${userData.token}`,
+	// 			"Content-Type": "application/json",
+	// 		},
+	// 	})
+	// 		.then((response) => {
+	// 			return response.json();
+	// 		})
+	// 		.then((responseData) => {
+	// 			setUsers(responseData);
+	// 		})
+	// 		.catch((err) => {
+	// 			console.log("error at fetching: ", err);
+	// 		});
+	// };
 
 	useEffect(() => {
-		loadUsers();
+		loadUsers(userData.token, signal).then((data) => {
+			setUsers(data);
+		});
+		return function cleanup() {
+			abortController.abort();
+		};
 		// eslint-disable-next-line
 	}, []);
 
--- Client/src/components/users/UserApi.js
+++ Client/src/components/users/UserApi.js
@@ -25,3 +25,22 @@ export const login = async (user) => {
 		console.log("error at fetching: ", err);
 	}
 };
+
+export const loadUsers = async (token, signal) => {
+	try {
+		const response = await fetch(
+			`${process.env.REACT_APP_API_SERVER_URL}/users`,
+			{
+				signal: signal,
+				method: "GET",
+				headers: {
+					Authorization: `Bearer ${token}`,
+					"Content-Type": "application/json",
+				},
+			}
+		);
+		return response.json();
+	} catch (err) {
+		console.log("error at fetching: ", err);
+	}
+};
